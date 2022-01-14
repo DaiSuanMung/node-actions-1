2d5780f523beb6438fc3ccba0d45fe516613121a
Dai Suan Mung <daisuanmung8@gmail.com>
2022-01-14 15:06:25 +0100

    edit tody

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.get("/",(req,res)=> res.send("Welcome to Docker Hub Image Registry"))
+app.get("/",(req,res)=> res.send("Welcome to Docker Hub Image Registry edit TOday"))
 
 app.listen(3000, ()=>{
     console.log("my port run in 3000")
